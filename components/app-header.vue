ca64ed428bb91b4a9d33fda822724c35196a4557
El-litios <carlosguzmanrojas00@gmail.com>
2024-05-27 21:42:40 -0400

    header of layout

---
+++ components/app-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="flex justify-between items-center py-6">
+    <NuxtLink to="/" class="text-xl font-bold">Finance Traker</NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
