1f758bcc48aac83d3e8659b308a23a91ca39189f
El-litios <carlosguzmanrojas00@gmail.com>
2024-05-27 21:42:47 -0400

    main layout

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mx-auto max-w-4xl">
+    <AppHeader></AppHeader>
+    <main class="my-10">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  link: [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+      href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap",
+      rel: "stylesheet",
+    },
+  ],
+});
+</script>
+
+<style>
+body {
+  font-family: 'Inter';
+}
+
+body{
+  @apply dark:bg-gray-900 bg-white
+}
+</style>
