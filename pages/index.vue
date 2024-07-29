7d43c79fd4da0e0d592eda23afc9a5516736f65e
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2024-07-29 16:57:23 -0400

    home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    
-  </div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu :options="transactionViewOptions" v-model="selectedView"></USelectMenu>
+    </div>
+  </section>  
+  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+    <Trend color="green" title="InCome" :amount="4000" :last-amount="2200" :loading="false"></Trend>
+    <Trend color="red" title="Expense" :amount="4000" :last-amount="2200" :loading="false"></Trend>
+    <Trend color="green" title="Investments" :amount="4000" :last-amount="2200" :loading="false"></Trend>
+    <Trend color="red" title="Savings" :amount="4000" :last-amount="4200" :loading="false"></Trend>
+  </section>
+
+  <section>
+    <Transaction></Transaction>
+    <Transaction></Transaction>
+    <Transaction></Transaction>
+  </section>
 </template>
 
 <script lang="ts" setup>
+import {transactionViewOptions} from '~/constants';
 
+const selectedView = ref(transactionViewOptions[1]);
 </script>
 
 <style>
