4d25cafbd1751f53315d8b3884386125787474f4
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2024-07-29 17:22:35 -0400

    added supabase library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"]
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
+
+  supabase: {
+    redirect: false
+  }
 })
